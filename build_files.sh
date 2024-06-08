75c53b7cb2fa3f3bc838b895cf3ddb5c884ade7b
jeffer-thebandit <jeff39724@gmail.com>
2024-06-08 20:52:31 +0300

    build

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@ python3.9 -m venv venv
 
 source venv/bin/activate
 
-python3.9 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
