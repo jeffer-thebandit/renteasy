48761a348e2314b8c2d460b5ecd21c720b2c7f30
jeffer-thebandit <jeff39724@gmail.com>
2024-06-11 16:28:00 +0300

    django

--- silk_road/settings.py
+++ silk_road/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-9hd+--irquy179!rv686a2)omdv*6@&(__ipe3pxx4=f0ke1v4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
